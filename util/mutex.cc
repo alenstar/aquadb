9190082f27cc186a8d352b128a4dfd705df76ffa
alenstar <to_alen@163.com>
2021-07-31 11:57:02 +0800

    update

--- util/mutex.cc
+++ util/mutex.cc
@@ -1,35 +1,35 @@
+#include <stdexcept>
 #include "mutex.h"
 
+namespace util
+{
 
-namespace util {
-
-Semaphore::Semaphore(uint32_t count) {
-    if(sem_init(&m_semaphore, 0, count)) {
+Semaphore::Semaphore(uint32_t count)
+{
+    if (sem_init(&m_semaphore, 0, count)) {
         throw std::logic_error("sem_init error");
     }
 }
 
-Semaphore::~Semaphore() {
-    sem_destroy(&m_semaphore);
-}
+Semaphore::~Semaphore() { sem_destroy(&m_semaphore); }
 
-int Semaphore::wait() {
+int Semaphore::wait()
+{
     int rc = sem_wait(&m_semaphore);
-    
-    if(rc != 0 ){
-        //throw std::logic_error("sem_wait error");
+
+    if (rc != 0) {
+        // throw std::logic_error("sem_wait error");
     }
     return rc;
 }
 
-int Semaphore::notify() {
+int Semaphore::notify()
+{
     int rc = sem_post(&m_semaphore);
-    if(rc != 0 ){
-        //throw std::logic_error("sem_post error");
+    if (rc != 0) {
+        // throw std::logic_error("sem_post error");
     }
     return rc;
 }
 
-
-
-}
+} // namespace util
